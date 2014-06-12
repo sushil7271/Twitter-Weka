c1d87414b1a79464f47dd9cfec53572db67362a4
SushilC <SushilC@WH35.whitehedge.com>
2014-06-12 18:40:41 +0530

    Completed

---
+++ TwitterReader/src/com/tmm/android/twitter/OnTaskCompleted.java
@@ -0,0 +1,9 @@
+package com.tmm.android.twitter;
+
+import java.util.ArrayList;
+
+import com.tmm.Twitter.Tweet;
+
+public interface OnTaskCompleted {
+	void onTaskCompleted(ArrayList<Tweet> Tweets_List);
+}
